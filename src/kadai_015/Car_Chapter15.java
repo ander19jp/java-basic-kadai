aacd4920a8c0e2865a09e41e757c8da79a4c0809
ander19jp <ander19jp@gmail.com>
2025-04-17 21:04:18 +0900

    課題提出

---
+++ src/kadai_015/Car_Chapter15.java
@@ -0,0 +1,34 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+
+	private int gear = 1; //初期ギア
+	private int speed = 10; //初期速度
+
+	//ギアチェンジメソッド
+	public void gearChange(int afterGear) {
+		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+		this.gear = afterGear;
+		
+		
+		//ギアが1～5の範囲なら、そのギアに応じた速度
+		if (afterGear >= 1 && afterGear <= 5) {
+			this.speed = afterGear * 10;
+		}else {
+			
+			//範囲外なら時速10km
+			this.speed = 10;
+		}
+		
+	}
+
+	//走行メソッド
+	public void run() {
+		System.out.println("速度は時差" + this.speed + "kmです");
+	}
+	
+	
+	
+	
+
+}
